b5070399d416535660846ec04fbe5d281f9c9135
CaicDev <caic.luiz.augustinho@gmail.com>
2022-12-27 11:35:31 +0000

    create model

---
+++ entity/character.go
@@ -0,0 +1,11 @@
+package entity
+
+type Character struct {
+	id           uint32
+	name         string
+	description  string
+	strenght     uint32
+	speed        uint32
+	intelligence uint32
+	xp           uint32
+}
