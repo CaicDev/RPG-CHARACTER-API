0ee8caefcb60f0fba181a2b01981d09d1869a45b
CaicDev <caic.luiz.augustinho@gmail.com>
2022-12-27 22:51:38 +0000

    capitalize the struct attributes

--- entity/character.go
+++ entity/character.go
@@ -1,11 +1,11 @@
 package entity
 
 type Character struct {
-	id           uint32 `json:"id"`
-	name         string `json:"name"`
-	description  string `json:"description"`
-	strength     uint32 `json:"strength"`
-	speed        uint32 `json:"speed"`
-	intelligence uint32 `json:"intelligence"`
-	xp           uint32 `json:"xp"`
+	ID           uint32 `json:"id"`
+	Name         string `json:"name"`
+	Description  string `json:"description"`
+	Magic        string `json:"magic"`
+	Strength     uint32 `json:"strength"`
+	Speed        uint32 `json:"speed"`
+	Intelligence uint32 `json:"intelligence"`
 }
