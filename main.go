f3563a9996d0ae7f2b093ec1cd15f7d676abfada
CaicDev <caic.luiz.augustinho@gmail.com>
2022-12-27 22:57:30 +0000

    create api group routes

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"rpg-api.com/m/controller"
+	"rpg-api.com/m/service"
+)
+
+
+var (
+	characterService service.CharacterService = service.New()
+	characterController controller.CharacterController = controller.New(characterService)
+)
 
 func main() {
 	server := gin.Default()
@@ -8,6 +18,13 @@ func main() {
 	server.GET("/", func(ctx *gin.Context) {
 		ctx.String(200, "Hello")
 	})
+
+	apiGroup := server.Group("/api")
+	{
+		apiGroup.GET("/character", characterController.FindAll)
+		apiGroup.GET("/character/:id", characterController.FindById)
+		apiGroup.POST("/character", characterController.Save)
+	}
 
 	server.Run("127.0.0.1:8080")
 }
