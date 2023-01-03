99a264b42c1cf2cdedd7bc722864c721fe13c0d6
CaicDev <caic.luiz.augustinho@gmail.com>
2023-01-03 00:40:11 +0000

    add data binding

--- entity/character.go
+++ entity/character.go
@@ -1,20 +1,20 @@
 package entity
 
 type Character struct {
-	ID           uint32 `json:"id"`
-	Name         string `json:"name"`
-	Description  string `json:"description"`
-	Magic        uint32 `json:"magic"`
-	Strength     uint32 `json:"strength"`
-	Speed        uint32 `json:"speed"`
-	Intelligence uint32 `json:"intelligence"`
+	ID           uint32 `json:"id" binding:"required"`
+	Name         string `json:"name" binding:"required"`
+	Description  string `json:"description" binding:"required"`
+	Magic        uint32 `json:"magic" binding:"required"`
+	Strength     uint32 `json:"strength" binding:"required"`
+	Speed        uint32 `json:"speed" binding:"required"`
+	Intelligence uint32 `json:"intelligence" binding:"required"`
 }
 
 type NewCharacter struct {
-	Name         string `json:"name"`
-	Description  string `json:"description"`
-	Magic        uint32 `json:"magic"`
-	Strength     uint32 `json:"strength"`
-	Speed        uint32 `json:"speed"`
-	Intelligence uint32 `json:"intelligence"`
+	Name         string `json:"name" binding:"required"`
+	Description  string `json:"description" binding:"required"`
+	Magic        uint32 `json:"magic" binding:"required"`
+	Strength     uint32 `json:"strength" binding:"required"`
+	Speed        uint32 `json:"speed" binding:"required"`
+	Intelligence uint32 `json:"intelligence" binding:"required"`
 }
